f5b0aa959903dcd94056ec3684c81e87962e580d
Code Yard <arduinoleo88@gmail.com>
2020-06-21 16:33:47 +0530

    Basic App with BluetoothUtils and Constantst

--- app/src/main/java/com/codeyard/aakraman3/MainActivity.java
+++ app/src/main/java/com/codeyard/aakraman3/MainActivity.java
@@ -1,25 +1,24 @@
 package com.codeyard.aakraman3;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    static String USER_ID;
     final int REQUEST_ENABLE_BT = 2;
+    final String TAG = MainActivity.class.getName();
     Context context;
     BluetoothAdapter bluetoothAdapter;
     SharedPreferences sharedPreferences;
-    static String USER_ID;
-
-    final String TAG = MainActivity.class.getName();
 
     @Override
     public void onCreate(Bundle saved) {
@@ -61,17 +60,7 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_ENABLE_BT) {
-            if (resultCode == RESULT_CANCELED) {
-                Log.d(TAG, "onActivityResult: user not turn bl on");
-//                TODO make a alert
-                finish();
-            }
-        }
-    }
+
 
     public String getUserId() {
         return sharedPreferences.getString(USER_ID, "");
@@ -86,6 +75,37 @@ public class MainActivity extends AppCompatActivity {
     public void showAlert(AlertDialog.Builder alertDialogBuilder) {
         AlertDialog alertDialog = alertDialogBuilder.create();
         alertDialog.show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Ensures Bluetooth is enabled on the device.  If Bluetooth is not currently enabled,
+        // fire an intent to display a dialog asking the user to grant permission to enable it.
+        if (!bluetoothAdapter.isEnabled()) {
+            if (!bluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // User chose not to enable Bluetooth.
+        if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED) {
+            finish();
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 }
 
